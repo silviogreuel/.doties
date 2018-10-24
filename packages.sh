99e662f3a6d309c842d1030e72007b1b7c0b93cb
Silvio Greuel <silviogreuel@gmail.com>
2018-10-23 23:26:08 -0300

    Adding base packages installation

---
+++ packages.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+
+set -e
+
+as_root=''
+[ $UID = 0 ] || as_root='sudo'
+
+info() {
+	printf "$(tput bold)$(tput setaf 4) -> $1$(tput sgr0)\n"
+}
+
+packages=(
+	# programming 
+	ctags
+	clang
+	make
+	cmake
+	git
+	tig
+	gcc
+	
+	#languages
+	go
+	ruby
+	lua
+	perl
+	python
+	python2
+	
+	# text editors 
+	emacs # search for spacemacs
+	vim
+	neovim
+	code
+
+	# terminal emulators
+	alacritty
+	kitty
+	rxvt-unicode
+
+	# window managers / utils
+	i3-gaps
+	i3status
+	dmenu
+	rofi
+	polybar
+
+	# applications / utils
+	htop
+	glances
+	ngrok
+	devtools
+	ranger
+	mutt
+	fzf
+	bat
+	asciinema
+	inetutils
+	net-tools
+	nmon
+	openssh
+	pulsemixer
+	vlc
+)
+
+install_from_pacman() {
+	packages=("$@")
+
+	to_install=()
+	for pack in "${packages[@]}"; do
+		pacman -Qq $pack > /dev/null 2>&1 || to_install+=("$pack")
+	done
+
+	if [ "${#to_install}" -gt 0 ]; then
+		$as_root pacman -Sy $to_install
+	else
+		info "Done installing all packages"
+	fi
+}
+
+install_from_pacman "${packages[@]}"
+
