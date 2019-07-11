0381c0b82732c79023942fd2bd35dade040fc4c2
Silvio Greuel <silviogreuel@gmail.com>
2019-07-10 21:25:03 -0300

    Allowing to stop only the ffmpeg gif recorder

--- .bin/start-gif.sh
+++ .bin/start-gif.sh
@@ -1,23 +1,11 @@
 #!/usr/bin/env bash 
 TMPFILE="$(mktemp -t screencast-XXXXXXX).mkv"
-OUTPUT="$HOME/Desktop/$(date +%F-%H-%M-%S)"
 
 #grab screen part
 slop=$(slop -f "%x %y %w %h %g %i") || exit 1
 read -r X Y W H G ID < <(echo $slop)
 
 #record using ffmpeg to tempfile
-ffmpeg -f x11grab -s "$W"x"$H" -i :0.0+$X,$Y "$TMPFILE"
+ffmpeg -f x11grab -s "$W"x"$H" -i :0.0+$X,$Y "$TMPFILE" & echo $! > /tmp/rec-gif.pid
 
-#generate pallet and convert to gif
-ffmpeg -y -i "$TMPFILE"  -vf fps=10,palettegen /tmp/palette.png
-ffmpeg -i "$TMPFILE" -i /tmp/palette.png -filter_complex "paletteuse" $OUTPUT.gif
-mv $TMPFILE $OUTPUT.mkv
-
-#copy to clipboard
-xclip -selection clipboard -t image/gif < $OUTPUT.gif
-
-#preview
-ffplay $OUTPUT.gif
-
-trap "rm -f '$TMPFILE'" 0
+echo $TMPFILE > /tmp/rec-gif.file
--- .bin/stop-gif.sh
+++ .bin/stop-gif.sh
@@ -1,2 +1,18 @@
 #!/usr/bin/env bash
-killall ffmpeg
+OUTPUT="$HOME/Desktop/$(date +%F-%H-%M-%S)"
+TMPFILE=`cat /tmp/rec-gif.file`
+
+pkill -F /tmp/rec-gif.pid
+
+#generate pallet and convert to gif
+ffmpeg -y -i "$TMPFILE"  -vf fps=10,palettegen /tmp/palette.png
+ffmpeg -i "$TMPFILE" -i /tmp/palette.png -filter_complex "paletteuse" $OUTPUT.gif
+mv $TMPFILE $OUTPUT.mkv
+
+#copy to clipboard
+xclip -selection clipboard -t image/gif < $OUTPUT.gif
+
+#preview
+ffplay -loop 0 -stats $OUTPUT.gif
+
+trap "rm -f '$TMPFILE'" 0
