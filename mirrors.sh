ae5f614aaa69db09d9da809cddae2b7f1dfc2cc1
Silvio Greuel <silviogreuel@gmail.com>
2021-05-12 04:24:23 -0300

    Using short name for filevllllll

---
+++ mirrors.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+curl -o /etc/pacman.d/mirrorlist.backup https://www.archlinux.org/mirrorlist/all/
+cp /etc/pacman.d/mirrorlist.backup /etc/pacman.d/mirrorlist
+reflector --verbose -l 200 -p http --sort rate --save /etc/pacman.d/mirrorlist
