b71749716b0a8d717f410803c151297615fc2b40
Silvio Greuel <silviogreuel@gmail.com>
2022-10-03 18:41:41 -0300

    Enforce treesitter to download elixir

--- .config/nvim/lua/setup/nvim-treesitter.lua
+++ .config/nvim/lua/setup/nvim-treesitter.lua
@@ -1,4 +1,6 @@
 require("nvim-treesitter.configs").setup({
+    ensure_installed = {"elixir"},
+    auto_install = true,
     highlight = {
       enable = true,
       additional_vim_regex_highlighting = true
